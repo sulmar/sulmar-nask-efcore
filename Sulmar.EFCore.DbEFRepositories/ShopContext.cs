413301e08fed801bfbd7704217c8ecbe90a4bf42
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-06-28 11:01:49 +0200

    Utworzenie DbContext

---
+++ Sulmar.EFCore.DbEFRepositories/ShopContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sulmar.EFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sulmar.EFCore.DbEFRepositories
+{
+
+    // dotnet package Microsoft.EntityFrameworkCore
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        // public DbSet<Item> Items { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Service> Services { get; set; }
+
+
+    }
+}
