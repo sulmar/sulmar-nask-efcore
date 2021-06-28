a77021211c77ac8d27673ebd6b2c37b37affabfb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-06-28 15:32:42 +0200

    Własne konwencje

--- Sulmar.EFCore.DbEFRepositories/Configurations/CustomerConfiguration.cs
+++ Sulmar.EFCore.DbEFRepositories/Configurations/CustomerConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using Sulmar.EFCore.Models;
 using System;
 using System.Collections.Generic;
@@ -7,18 +8,21 @@ using System.Text;
 
 namespace Sulmar.EFCore.DbEFRepositories.Configurations
 {
+    
+   
+
     class CustomerConfiguration : IEntityTypeConfiguration<Customer>
     {
         public void Configure(EntityTypeBuilder<Customer> builder)
         {
             builder.HasKey(p => p.Id);
 
-            builder.Property(p => p.FirstName)
-                .HasMaxLength(50);
+            //builder.Property(p => p.FirstName)
+            //    .HasMaxLength(50);
 
-            builder
-                .Property(p => p.LastName)
-                .HasMaxLength(50);
+            //builder
+            //    .Property(p => p.LastName)
+            //    .HasMaxLength(50);
 
             builder
                 .Property(p => p.Pesel)
--- Sulmar.EFCore.DbEFRepositories/ShopContext.cs
+++ Sulmar.EFCore.DbEFRepositories/ShopContext.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Sulmar.EFCore.DbEFRepositories
 {
@@ -31,17 +33,44 @@ namespace Sulmar.EFCore.DbEFRepositories
             base.OnModelCreating(modelBuilder);
 
 
+            // Custom convention
+            // https://docs.microsoft.com/en-us/dotnet/api/microsoft.entityframeworkcore.metadata.conventions?view=efcore-5.0
+
+            var properties = modelBuilder.Properties<string>()
+                      .Where(p => p.Name.Contains("Name"));
+
+            foreach (var property in properties)
+            {
+                property.SetMaxLength(100);
+                property.IsNullable = false;                
+            }
 
             // Fluent Api
 
             // modelBuilder.ApplyConfiguration(new ItemConfiguration());
             //modelBuilder.ApplyConfiguration(new CustomerConfiguration());
             //modelBuilder.ApplyConfiguration(new ProductConfiguration());
-            
+
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
         }
 
 
+    }
+
+    public static class ModelBuilderExtensions
+    {
+        public static IEnumerable<IMutableProperty> Properties(this ModelBuilder modelBuilder)
+        {
+            var properties = from e in modelBuilder.Model.GetEntityTypes()
+                             from p in e.GetProperties()                             
+                             select p;
+
+            return properties;
+        }
+
+        public static IEnumerable<IMutableProperty> Properties<T>(this ModelBuilder modelBuilder)
+             => modelBuilder.Properties().Where(p => p.PropertyInfo.PropertyType == typeof(T));
+        
     }
 }
