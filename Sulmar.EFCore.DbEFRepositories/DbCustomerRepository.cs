b3ca4d2108c82770765a732a843c74e98b15b0eb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-06-28 10:28:19 +0200

    DbCustomerRepository

--- Sulmar.EFCore.DbEFRepositories/DbCustomerRepository.cs
+++ Sulmar.EFCore.DbEFRepositories/DbCustomerRepository.cs
@@ -1,8 +1,40 @@
-﻿using System;
+﻿using Sulmar.EFCore.IRepositories;
+using Sulmar.EFCore.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Sulmar.EFCore.DbEFRepositories
 {
-    public class DbCustomerRepository
+    public class DbCustomerRepository : ICustomerRepository
     {
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(string pesel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
